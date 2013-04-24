dcec8e7f9241a890a4c3556ee2367bf4baf3021f
Matthew Moore <matt@crowdmob.com>
2013-04-24 09:51:51 -0700

    include_attribute "deploy::default"

--- attributes/run.rb
+++ attributes/run.rb
@@ -1,3 +1,4 @@
+include_attribute "deploy::default"
 
 default[:kafka_s3_consumer][:_application_defaults][:kafka_topic_array] = ["example-topic-set-by-chef"]
 default[:kafka_s3_consumer][:_application_defaults][:kafka_max_message_size] = 4096
@@ -8,9 +9,9 @@ default[:kafka_s3_consumer][:_application_defaults][:filebufferpath_base] = "/mn
 default[:kafka_s3_consumer][:_application_defaults][:pollsleepmillis] = 10
 
 node[:deploy].each do |application, _|
-  if node[:deploy][application][:environment]["HOME"] && node[:deploy][application][:env]
+  if node[:deploy][application][:environment] && node[:deploy][application][:environment]["HOME"] && node[:deploy][application][:env]
     default[:kafka_s3_consumer][application][:env] = {"HOME" => node[:deploy][application][:environment]["HOME"]}.merge(node[:deploy][application][:env])
-  elsif node[:deploy][application][:environment]["HOME"]
+  elsif node[:deploy][application][:environment] && node[:deploy][application][:environment]["HOME"]
     default[:kafka_s3_consumer][application][:env] = {"HOME" => node[:deploy][application][:environment]["HOME"]}
   elsif node[:deploy][application][:env]
     default[:kafka_s3_consumer][application][:env] = node[:deploy][application][:env]
@@ -27,6 +28,6 @@ node[:deploy].each do |application, _|
   default[:kafka_s3_consumer][application][:config_file] = "#{node[:kafka_s3_consumer][:config_files_path]}/kafka_s3_consumer_#{application}.properties"
   default[:kafka_s3_consumer][application][:pid_file] = "#{node[:kafka_s3_consumer][:pid_files_path]}/kafka_s3_consumer_#{application}.pid"
   default[:kafka_s3_consumer][application][:output_file] = "#{node[:kafka_s3_consumer][:output_files_path]}/kafka_s3_consumer_#{application}.out"
-  default[:kafka_s3_consumer][application][:kafka_topic_array] = node[:deploy][application][:kafka][:topics] || default[:kafka_s3_consumer][:_application_defaults][:topic_array]
-  default[:kafka_s3_consumer][application][:kafka_max_message_size] = node[:deploy][application][:kafka][:max_message_size] || default[:kafka_s3_consumer][:_application_defaults][:kafka_max_message_size]
+  default[:kafka_s3_consumer][application][:kafka_topic_array] = (node[:deploy][application][:kafka] && node[:deploy][application][:kafka][:topics]) || default[:kafka_s3_consumer][:_application_defaults][:topic_array]
+  default[:kafka_s3_consumer][application][:kafka_max_message_size] = (node[:deploy][application][:kafka] && node[:deploy][application][:kafka][:max_message_size]) || default[:kafka_s3_consumer][:_application_defaults][:kafka_max_message_size]
 end
